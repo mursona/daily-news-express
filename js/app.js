e2faf1ad907cd0833c408ebe9f8bdca6ec00c74a
mursona <mursona>
2022-09-03 01:03:38 +0600

    category load from api

--- js/app.js
+++ js/app.js
@@ -2,11 +2,22 @@ const customColor = document.getElementById('custom-color')
 customColor.style.color = '#0000FD';
 
 
-const loadPhone = async() => {
+const loadNews = async() => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     const res = await fetch(url);
-    const data = await res.json();
-    console.log(data.data);
-    // displayPhone(data.data, dataLimit);
+    const data = await res.json();;
+    displayNews(data.data.news_category);
 }
-loadPhone()
+const displayNews = (categories) => {
+  const displayCategory = document.getElementById('category');
+
+  categories.forEach(category => {
+      const categoryDiv = document.createElement('li');
+      categoryDiv.classList.add('nav-item', 'd-block');
+      categoryDiv.innerHTML = `
+      <a class="nav-link disabled" href="#">${category.category_name}</a>`;
+      displayCategory.appendChild(categoryDiv);
+  });
+}
+loadNews();
+
